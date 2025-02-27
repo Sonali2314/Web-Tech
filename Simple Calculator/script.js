456f1955aa027c733a2d0abae71cdaf77ef36c2e
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2025-02-27 10:19:34 +0530

    Add files via upload

---
+++ Simple Calculator/script.js
@@ -0,0 +1,27 @@
+let resultField = document.getElementById("result");
+let currentInput = "";
+
+function appendNumber(number) {
+  currentInput += number;
+  resultField.value = currentInput;
+}
+
+function appendOperator(operator) {
+  currentInput += ` ${operator} `;
+  resultField.value = currentInput;
+}
+
+function clearResult() {
+  currentInput = "";
+  resultField.value = "";
+}
+
+function calculateResult() {
+  try {
+    currentInput = eval(currentInput);
+    resultField.value = currentInput;
+  } catch (error) {
+    resultField.value = "Error";
+    currentInput = "";
+  }
+}
